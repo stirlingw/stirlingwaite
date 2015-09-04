f2e17e169e149c60df0ee684358517af672adfd7
stirlingw <Lyman123>
2015-09-04 00:31:00 -0600

    adding things to build

---
+++ build/1.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([1],{
+
+/***/ 20:
+/***/ function(module, exports) {
+
+	module.exports = "<div id=\"home-view\">\n    <!-- Page Header -->\n    <!-- Set your background image for this header on the line below. -->\n    <header class=\"intro-header\" style=\"background-image: url('./shared/img/home-bg.jpg')\">\n        <div class=\"container\">\n            <div class=\"row\">\n                <div class=\"col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1\">\n                    <div class=\"site-heading\">\n                        <h1>Stirling Waite</h1>\n                        <hr class=\"small\">\n                        <span class=\"subheading\">The Man.  The myth.  The Legend?</span>\n                    </div>\n                </div>\n            </div>\n        </div>\n    </header>\n\n    <!-- Main Content -->\n    <blog-posts-directive></blog-posts-directive>\n</div>";
+
+/***/ }
+
+});
---
+++ build/4.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([4],{
+
+/***/ 23:
+/***/ function(module, exports) {
+
+	module.exports = "<!-- Page Header -->\n<!-- Set your background image for this header on the line below. -->\n<header class=\"intro-header\" style=\"background-image: url('./shared/img/post-bg.jpg')\">\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1\">\n                <div class=\"post-heading\">\n                    <h1>{{blog.title}}</h1>\n                    <h2 class=\"subheading\">{{blog.subTitle}}</h2>\n                    <span class=\"meta\">Posted by <a href=\"#\">Stirling</a> on {{blog.date}}</span>\n                </div>\n            </div>\n        </div>\n    </div>\n</header>\n\n<!-- Post Content -->\n<article>\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1\">\n                <div ng-bind-html=\"fileContent\"></div>\n            </div>\n        </div>\n    </div>\n</article>";
+
+/***/ }
+
+});
